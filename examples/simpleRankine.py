f23c4c3d1f02a198ca4ee971e09af1a8e3b7f707
Stu Blair <stu314159@gmail.com>
2017-08-15 13:32:20 -0400

    added a simple Rankine power cycle example

--- examples/simpleRankine.py
+++ examples/simpleRankine.py
@@ -28,6 +28,7 @@ myFluid = EP.EasyProp('Water','SI')
 numStatePoints = 4;
 eta_turbine = 0.88;
 eta_pump = 0.82;
+m_dot_steam = 2.8e4; # kg/h
 
 # declare fluid property arrays
 h = np.zeros((numStatePoints,),dtype=np.float)
@@ -38,8 +39,72 @@ P = np.zeros_like(h);
 T = np.zeros_like(h);
 T_s = np.zeros_like(h);
 
-# state point 1: condensate pump inlet
+# state point 0: condensate pump inlet
 P[0] = 7.5; # given pressure at condenser outlet / condensate pump inlet
 h[0] = myFluid.hL_p(P[0])
 s[0] = myFluid.sL_p(P[0])
+
+# compression from state point 0 to 1
+P[1] = 8000.; # boiler pressure of 8 MPa given
+s_s[1] = s[0]
+h_s[1] = myFluid.h_ps(P[1],s_s[1]) # isentropic compression
+h[1] = h[0] - (h[0] - h_s[1])/eta_pump; #<-- correct for pump efficiency
+w_pump = h[0] - h[1] # real pump specific work (kJ/kg)
+
+# isobaric heat addition in the boiler from state point 1 to 2
+P[2] = P[1]
+h[2] = myFluid.hV_p(P[2]) #<-- saturated vapor leaves the boiler (given)
+s[2] = myFluid.sV_p(P[2])
+
+q_s = h[2] - h[1] #<-- specific heat supplied (kJ/kg)
+
+# expansion from state point 2 to 3 in the turbine
+s_s[3] = s[2]
+P[3] = P[0]
+h_s[3] = myFluid.h_ps(P[3],s_s[3])
+h[3] = h[2] - (h[2] - h_s[3])*eta_turbine
+w_turbine = h[2] - h[3] # real turbine specific work (kJ/kg)
+
+# isobaric heat rejection in the condenser from state point 3 to 0
+q_r = h[0] - h[3] #<-- specific heat rejected (kJ/kg)
+
+w_net = w_pump + w_turbine;
+q_net = q_s + q_r;
+
+# check energy balance; w_net should equal q_net
+print "Net specific work = %g kJ/kg "%w_net
+print "Net specific heat = %g kJ/kg "%q_net
+
+
+# answer some questions:
+
+#a) net power produced = w_net*m_dot_stm
+net_power = w_net*m_dot_steam*(1./3600.) #(kW = kJ/s)
+
+print "Net power = %g kW"%net_power
+
+#b) net heat transfer rate into the boiler
+net_Qdot_supplied = q_s*m_dot_steam*(1./3600.) #kW =  kJ/s
+
+print "Net heat transfer rate into the boiler = %g kW"%net_Qdot_supplied
+
+#c) cycle thermal efficiency
+eta_th = w_net/q_s 
+print "Thermal Efficiency = %4.1f percent "%(eta_th*100.)
+
+#d) cooling water flow rate.
+
+# state point 4 = condenser cooling water inlet condition
+Po = 101.325 # kPa assume standard atmospheric pressure for condenser cooling water
+Tcond_in = 20. #C - given
+Tcond_out = 38. #C - given
+h_cond_in = myFluid.h_pT(Po,Tcond_in)
+h_cond_out = myFluid.h_pT(Po,Tcond_out)
+
+# energy balance on condenser for cooling water flow rate
+m_dot_cool = m_dot_steam*(h[3]-h[0])/(h_cond_out - h_cond_in); #kg/h
+
+print "Condenser cooling water flow rate = %g kg/h"%m_dot_cool
+
+
 
