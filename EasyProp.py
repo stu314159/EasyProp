702720819d1f2a49a0117d36956215fd729adc92
Stu Blair <stu314159@gmail.com>
2017-08-23 17:18:32 -0400

    fixed bug in temperature conversion for Psat_t()

--- EasyProp.py
+++ EasyProp.py
@@ -376,7 +376,7 @@ class EasyProp(object):
         """
         
         if self.ConvertUnits==False:
-            T-=273.15 # convert C to K
+            T+=273.15 # convert C to K
         else:
             T = self.converter.F_toK(T)
             
